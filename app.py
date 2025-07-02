f34621f1f331da7e2a5dc3cf3efd52fe65bf5601
myfiorentina <amobile.info@gmail.com>
2025-07-02 13:16:35 +0200

    Aggiunta campo email al registro utenti

--- app.py
+++ app.py
@@ -757,4 +757,5 @@ def elimina_utente(username):
 # ======================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
